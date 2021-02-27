3fc1822730ac01c25b4347eb400655f527c7267a
Marek Bartoš <bartos.developer152@gmail.com>
2021-02-27 18:03:04 +0100

    EnumWrapper

---
+++ src/ORM/Wrapper/EnumWrapper.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+namespace OriCMF\Core\ORM\Wrapper;
+
+use MabeEnum\Enum;
+use Nextras\Orm\Entity\ImmutableValuePropertyWrapper;
+use Orisai\Exceptions\Logic\InvalidArgument;
+use function array_key_first;
+use function assert;
+use function count;
+use function is_string;
+use function is_subclass_of;
+
+final class EnumWrapper extends ImmutableValuePropertyWrapper
+{
+
+	/**
+	 * @param mixed $value
+	 * @return mixed $value
+	 */
+	public function convertToRawValue($value)
+	{
+		assert($value instanceof Enum);
+
+		return $value->getName();
+	}
+
+	/**
+	 * @param mixed $value
+	 */
+	public function convertFromRawValue($value): Enum
+	{
+		assert(is_string($value));
+
+		$types = $this->propertyMetadata->types;
+
+		if (count($types) !== 1) {
+			throw InvalidArgument::create()
+				->withMessage('Property must have one and only one type defined.');
+		}
+
+		$type = array_key_first($types);
+		assert(is_subclass_of($type, Enum::class));
+
+		return $type::byName($value);
+	}
+
+}
