11bab0bcf82aee5f3e5ff04d5d233ad23165056e
Marek Bartoš <bartos.developer152@gmail.com>
2021-02-28 16:10:31 +0100

    UserRepository->findByPrivilege()

--- src/User/UserRepository.php
+++ src/User/UserRepository.php
@@ -2,10 +2,14 @@
 
 namespace OriCMF\Core\User;
 
+use Nextras\Orm\Collection\ICollection;
 use Nextras\Orm\Entity\Entity;
 use Nextras\Orm\Repository\IDependencyProvider;
 use OriCMF\Core\ORM\BaseRepository;
+use OriCMF\Core\ORM\Functions\JsonAnyKeyOrValueExistsFunction;
 use OriCMF\Core\User\Mapper\UserMapper;
+use Orisai\Auth\Authorization\Authorizer;
+use function explode;
 
 final class UserRepository extends BaseRepository
 {
@@ -21,6 +25,37 @@ final class UserRepository extends BaseRepository
 	public static function getEntityClassNames(): array
 	{
 		return [User::class];
+	}
+
+	/**
+	 * @return ICollection&iterable<User>
+	 */
+	public function findByPrivilege(string $privilege): ICollection
+	{
+		return $this->findBy([
+			JsonAnyKeyOrValueExistsFunction::class,
+			'roles->privileges',
+			$this->getPrivilegeParents($privilege),
+		]);
+	}
+
+	/**
+	 * @return array<string>
+	 */
+	private function getPrivilegeParents(string $privilege): array
+	{
+		$parts = explode('.', $privilege);
+		$all = [
+			Authorizer::ALL_PRIVILEGES,
+		];
+		$current = null;
+
+		foreach ($parts as $part) {
+			$current = $current === null ? $part : "{$current}.{$part}";
+			$all[] = $current;
+		}
+
+		return $all;
 	}
 
 }
