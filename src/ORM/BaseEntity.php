f5fbe0f16536fa22397282fa563d3cc7b8e61ff6
Marek Bartoš <bartos.developer152@gmail.com>
2021-04-05 17:51:11 +0200

    ORM: BaseEntity

---
+++ src/ORM/BaseEntity.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace OriCMF\Core\ORM;
+
+use Nextras\Orm\Entity\Entity;
+
+abstract class BaseEntity extends Entity
+{
+
+	/**
+	 * @return $this
+	 */
+	public function setModifiedValue(string $name, mixed $value): self
+	{
+		if (!$this->hasValue($name) || $this->getValue($name) !== $value) {
+			$this->setValue($name, $value);
+		}
+
+		return $this;
+	}
+
+	public function __set(string $name, mixed $value): void
+	{
+		$this->setModifiedValue($name, $value);
+	}
+
+}
