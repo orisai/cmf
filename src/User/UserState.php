2620f90be9e6eff11b6ff9735cd9d3cbde29818e
Marek Bartoš <bartos.developer152@gmail.com>
2021-05-17 20:13:06 +0200

    Move orisai/localization from orisai/cmf-ui, UserState translation

--- src/User/UserState.php
+++ src/User/UserState.php
@@ -8,6 +8,7 @@ use MabeEnum\Enum;
  * @method static self NEW()
  * @method static self ACTIVE()
  * @method static self DISABLED()
+ * @method string getValue()
  */
 final class UserState extends Enum
 {
@@ -17,5 +18,24 @@ final class UserState extends Enum
 	public const ACTIVE = 'active';
 
 	public const DISABLED = 'disabled';
+
+	private const LABELS = [
+		self::NEW => 'ori.ui.user.state.new',
+		self::ACTIVE => 'ori.ui.user.state.active',
+		self::DISABLED => 'ori.ui.user.state.disabled',
+	];
+
+	public function getLabel(): string
+	{
+		return self::LABELS[$this->getValue()];
+	}
+
+	/**
+	 * @return array<string, string>
+	 */
+	public static function getValueLabelPairs(): array
+	{
+		return self::LABELS;
+	}
 
 }
