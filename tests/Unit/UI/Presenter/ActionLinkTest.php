97bceb594d103603b62077af4481b3296c0952f9
Marek Bartoš <bartos.developer152@gmail.com>
2022-01-07 21:15:59 +0100

    ActionLink: tests

---
+++ tests/Unit/UI/Presenter/ActionLinkTest.php
@@ -0,0 +1,125 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\OriCMF\Unit\UI\Presenter;
+
+use Generator;
+use OriCMF\UI\Presenter\ActionLink;
+use OriCMF\UI\Presenter\Base\BasePresenter;
+use Orisai\Exceptions\Logic\InvalidArgument;
+use PHPUnit\Framework\TestCase;
+
+final class ActionLinkTest extends TestCase
+{
+
+	public function testClass(): void
+	{
+		$class = BasePresenter::class;
+
+		$link = ActionLink::fromClass($class);
+		self::assertSame(
+			$link->getDestination(),
+			":$class:default",
+		);
+		self::assertSame(
+			[],
+			$link->getArguments(),
+		);
+
+		$link = ActionLink::fromClass($class, ['a' => 'b'], 'foo');
+		self::assertSame(
+			$link->getDestination(),
+			":$class:foo",
+		);
+		self::assertSame(
+			['a' => 'b'],
+			$link->getArguments(),
+		);
+	}
+
+	public function testMapping(): void
+	{
+		$link = ActionLink::fromMapping(':Foo:Bar:baz');
+		self::assertSame(
+			$link->getDestination(),
+			':Foo:Bar:baz',
+		);
+		self::assertSame(
+			[],
+			$link->getArguments(),
+		);
+
+		$link = ActionLink::fromMapping('//:Foo:Bar:baz', ['a' => 'b']);
+		self::assertSame(
+			$link->getDestination(),
+			'//:Foo:Bar:baz',
+		);
+		self::assertSame(
+			['a' => 'b'],
+			$link->getArguments(),
+		);
+	}
+
+	public function testAnchor(): void
+	{
+		$link = ActionLink::fromMapping(':Foo:Bar:baz');
+		$link->setAnchor('anchor');
+		self::assertSame(
+			$link->getDestination(),
+			':Foo:Bar:baz#anchor',
+		);
+
+		$class = BasePresenter::class;
+		$link = ActionLink::fromClass($class);
+		$link->setAnchor('anchor');
+		self::assertSame(
+			$link->getDestination(),
+			":$class:default#anchor",
+		);
+	}
+
+	public function testAddArgument(): void
+	{
+		$link = ActionLink::fromClass(BasePresenter::class, ['a' => 1]);
+		self::assertSame(
+			[
+				'a' => 1,
+			],
+			$link->getArguments(),
+		);
+
+		$link->addArgument('a', 2);
+		$link->addArgument('b', 3);
+		self::assertSame(
+			[
+				'a' => 2,
+				'b' => 3,
+			],
+			$link->getArguments(),
+		);
+	}
+
+	/**
+	 * @dataProvider provideForbidden
+	 */
+	public function testForbidden(string $destination): void
+	{
+		$this->expectException(InvalidArgument::class);
+		$this->expectExceptionMessage(<<<'MSG'
+Destination must be an absolute link. Relative links and "this" are forbidden.
+Format: [[[module:]presenter:]action | signal!] [#fragment]
+MSG);
+
+		ActionLink::fromMapping($destination);
+	}
+
+	/**
+	 * @return Generator<array<mixed>>
+	 */
+	public function provideForbidden(): Generator
+	{
+		yield ['this'];
+		yield ['//this'];
+		yield ['Foo:Bar:baz'];
+	}
+
+}
